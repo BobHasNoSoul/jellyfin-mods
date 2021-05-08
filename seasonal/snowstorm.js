f2821340ab3bbe86671a8f1cf6fdeb9adeb77367
BobHasNoSoul <antonyedwards93@gmail.com>
2021-05-08 20:29:41 +0100

    Add files via upload

--- seasonal/snowstorm.js
+++ seasonal/snowstorm.js
@@ -43,7 +43,7 @@ var snowStorm = (function(window, document) {
   this.flakeHeight = 8;           // Max pixel height reserved for snow element
   this.vMaxX = 5;                 // Maximum X velocity range for snow
   this.vMaxY = 4;                 // Maximum Y velocity range for snow
-  this.zIndex = -1;                // CSS stacking order applied to each snowflake
+  this.zIndex = 0;                // CSS stacking order applied to each snowflake
 
   // --- "No user-serviceable parts inside" past this point, yadda yadda ---
 
